9aae94f3de23d37b0b9d10e941343c5e474a40d1
Sol <johndoe@example.com>
2015-07-30 18:29:48 +0000

    init commit

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,50 @@
+class ArtistsController < ApplicationController
+
+  def index
+    @artists = Artist.all
+  end
+
+  def show
+    @artist = Artist.find_by(id: params[:id])
+    @songs = Song.where(artist_id: @artist.id)
+  end
+
+  def new
+  end
+
+  def create
+    @artist = Artist.new
+    @artist.name = params[:name]
+    @artist.image_url = params[:image_url]
+
+    if @artist.save
+      redirect_to "/artists/#{ @artist.id }"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @artist = Artist.find_by(id: params[:id])
+  end
+
+  def update
+    @artist = Artist.find_by(id: params[:id])
+    @artist.name = params[:name]
+    @artist.image_url = params[:image_url]
+
+    if @artist.save
+      redirect_to "/artists/#{ @artist.id }"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @artist = Artist.find_by(id: params[:id])
+    @artist.destroy
+
+
+    redirect_to "/artists"
+  end
+end
---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,49 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    @artist = Artist.find_by(id: @song.artist_id)
+  end
+
+  def new
+  end
+
+  def create
+    @song = Song.new
+    @song.name = params[:name]
+    @song.artist_id = params[:artist_id]
+    if @song.save
+      redirect_to "/songs/#{ @song.id }"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @song = Song.find_by(id: params[:id])
+  end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.name = params[:name]
+    @song.artist_id = params[:artist_id]
+
+    if @song.save
+      redirect_to "/songs/#{ @song.id }"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @song = Song.find_by(id: params[:id])
+    @song.destroy
+
+
+    redirect_to "/songs"
+  end
+end
